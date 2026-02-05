6bfbc51f0c1760dbabda2aea30ccff7dcba538c9
Kunzhao Ren <rkzdtc@gmail.com>
2026-02-05 04:08:00 -0600

    Refactor get_torch.sh and run.sh to improve environment variable setup for x86_64 architecture

--- scripts/get_torch.sh
+++ scripts/get_torch.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y libgfortran5 libopenblas0
+
 PY_VERSION="${1:-}"
 WHL_LINK="${2:-}"
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/../
 echo "Project directory: $PROJECT_DIR"
 
-sudo apt-get install -y libgfortran5 libopenblas0
-
 cd $PROJECT_DIR
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-uv venv --python "$PY_VERSION" --clear
-uv pip install numpy 
 
-if [[ -n "$WHL_LINK" ]]; then
-  echo "Installing torch from input: $WHL_LINK"
-  uv pip install "$WHL_LINK"
+if [[ "$(uname -m)" == "x86_64" ]]; then
+
+  mkdir -p thirdparty
+  cd thirdparty
+  wget -nc https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.10.0%2Bcpu.zip
+  unzip -n libtorch-shared-with-deps-2.10.0+cpu.zip
+
 else
-  echo "No input provided, installing default torch from PyPI..."
-  uv pip install torch
+
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+  source $HOME/.local/bin/env
+  uv venv --python "$PY_VERSION" --clear
+  uv pip install numpy 
+
+  if [[ -n "$WHL_LINK" ]]; then
+    echo "Installing torch from input: $WHL_LINK"
+    uv pip install "$WHL_LINK"
+  else
+    echo "No input provided, installing default torch from PyPI..."
+    uv pip install torch
+  fi
+
 fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -14,8 +14,17 @@ shift
 source "$PROJECT_DIR/../unitree_lowlevel/scripts/setup.sh" "$NET_IF" "${ROS_DISTRO:-}"
 
 cd "$PROJECT_DIR"
-export CMAKE_PREFIX_PATH="$(uv run python3 -c 'import torch; print(torch.utils.cmake_prefix_path)'):${CMAKE_PREFIX_PATH}"
-export LD_LIBRARY_PATH="$(uv run python3 -c 'import torch, pathlib; p=pathlib.Path(torch.__file__).resolve().parent; print(p / "lib")'):${LD_LIBRARY_PATH}"
+if [[ "$(uname -m)" == "x86_64" ]]; then
+
+  export CMAKE_PREFIX_PATH=thirdparty/libtorch:$CMAKE_PREFIX_PATH
+  export LD_LIBRARY_PATH=thirdparty/libtorch/lib:$LD_LIBRARY_PATH
+
+else
+
+  export CMAKE_PREFIX_PATH="$(uv run python3 -c 'import torch; print(torch.utils.cmake_prefix_path)'):${CMAKE_PREFIX_PATH}"
+  export LD_LIBRARY_PATH="$(uv run python3 -c 'import torch, pathlib; p=pathlib.Path(torch.__file__).resolve().parent; print(p / "lib")'):${LD_LIBRARY_PATH}"
+
+fi
 
 echo "CMAKE_PREFIX_PATH: $CMAKE_PREFIX_PATH"
 echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
